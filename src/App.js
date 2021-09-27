458bd50b1c5cabe6cd1d4f30f0bb799d0e16b87d
Mitchell Robles <mitchellr92@live.com>
2021-09-27 05:59:02 -0700

    countdown timer functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,36 @@ function App() {
   const [timerMinutes, setTimerMinutes] = useState();
   const [timerSeconds, setTimerSeconds] = useState();
 
+  let interval;
+
+  const startTimer = () => {
+    const countDownDate = new Date("October 1, 2022").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const day = Math.floor(distance / 1000 / 60 / 60 / 24);
+      const hour = Math.floor(distance / 1000 / 60 / 60) % 24;
+      const minute = Math.floor(distance / 1000 / 60) % 60;
+      const second = Math.floor(distance / 1000) % 60;
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(day);
+        setTimerHours(hour);
+        setTimerMinutes(minute);
+        setTimerSeconds(second);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
+
   return (
     <div className="App">
       Welcome to the Holy Frijole Chili Cook Off!
