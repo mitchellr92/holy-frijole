2d6da52c44535e8784d4e4e976eb5dd005df32f0
Mitchell Robles <mitchellr92@live.com>
2021-09-26 20:33:26 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countdown from "./components/utils/Countdown";
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to the Holy Frijole Chili Cook Off!
+      <Countdown />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/utils/Countdown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Countdown = () => {
+  return (
+    <div>
+      This is the countdown
+    </div>
+  )
+}
+
+export default Countdown;
