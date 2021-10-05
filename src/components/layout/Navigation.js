c856740e3f9110d41ae26c37aa6cbb33e0b3b458
Mitchell Robles <mitchellr92@live.com>
2021-10-05 16:27:55 -0700

    redoing navbar

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,43 +1,13 @@
 import React from "react";
-import { Navbar, Nav, Container, Link } from "react-bootstrap";
 
 const Navigation = () => {
   return (
-    // <Navbar bg="primary">
-    //   <Container >
-    //     <Navbar.Brand href="#" className="text-white">Holy Frijole Chili Cook Off</Navbar.Brand>
-    //     <Nav className="me-auto">
-    //       <Nav.Link href="#" className="text-white">Link 1</Nav.Link>
-    //       <Nav.Link href="#" className="text-white">Link 2</Nav.Link>
-    //       <Nav.Link href="#" className="text-white">Link 3</Nav.Link>
-    //     </Nav>
-    //     <Nav className="justify-content-end">
-    //       <Nav.Link href="#" className="text-white">
-    //         <i className="fas fa-sign-in-alt"></i> Sign In
-    //       </Nav.Link>
-    //       <Nav.Link href="#" className="text-white">
-    //         <i className="fas fa-user-plus"></i> Create Account
-    //       </Nav.Link>
-    //     </Nav>
-    //   </Container>
-    // </Navbar>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="d-flex">
+          <a className="navbar-brand" href="#">
+            Holy Frijole Chili Cook Off
+          </a>
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
@@ -54,14 +24,18 @@ const Navigation = () => {
                 Pricing
               </a>
             </li>
+          </ul>
+        </div>
+        <div className="d-flex">
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
+              <a href="#!" className="nav-link">
+                <i className="fas fa-sign-in-alt"></i> Sign In
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#!" className="nav-link">
+                <i className="fas fa-user-plus"></i> Create Account
               </a>
             </li>
           </ul>
