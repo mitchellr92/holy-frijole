94ce6e0816fa35665e0ea7694d7579aa4aebd34a
Mitchell Robles <mitchellr92@live.com>
2021-10-09 06:09:15 -0700

    setting up auth state

---
+++ src/context/auth/AuthState.js
@@ -0,0 +1,27 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import AuthContext from "./AuthContext";
+import AuthReducer from "./AuthReducer";
+import setAuthToken from "../../components/utils/setAuthToken";
+import {
+  REGISTER_SUCCESs,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from "../types";
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer{AuthReducer, initialState}
+};
