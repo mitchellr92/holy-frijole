1d092152015084544e5826a9d73f7c4bc5468f1e
Mitchell Robles <mitchellr92@live.com>
2021-10-12 18:12:28 -0700

    temporary fix for deploy

--- src/components/layout/Carousel.js
+++ src/components/layout/Carousel.js
@@ -1,28 +1,11 @@
-import React, { useContext } from "react";
-// import { Link } from "react-router-dom";
-// import AuthContext from "../../context/auth/AuthContext";
+import React from 'react'
 import chili1 from "../utils/photos/chili/chili1.jpg";
 import chili2 from "../utils/photos/chili/chili2.jpg";
 import chili3 from "../utils/photos/chili/chili3.jpg";
 import chili4 from "../utils/photos/chili/chili4.jpg";
 
-const Landing = () => {
-  // const authContext = useContext(AuthContext);
-
-  // const { isAuthenticated } = authContext;
-
-  // const authLinks = (
-  //   <div className="d-flex flex-column">
-  //     <Link to="/login">
-  //       <button className="btn btn-danger w-25 mb-3">Login</button>
-  //     </Link>
-  //     <Link to="/register">
-  //       <button className="btn btn-danger w-25">Create Account</button>
-  //     </Link>
-  //   </div>
-  // );
-
-  const carousel = (
+const Carousel = () => {
+  return (
     <div
       id="carouselExampleFade"
       className="carousel slide carousel-fade w-100"
@@ -43,19 +26,7 @@ const Landing = () => {
         </div>
       </div>
     </div>
-  );
+  )
+}
 
-  return (
-    <div className="d-flex flex-column justify-content-center align-items-center">
-      {/* <div className="landing border border-danger m-5">
-        <h2 className="d-flex justify-content-center">
-          Welcome to the Holy Frijole Chili Cook Off!
-        </h2>
-        <div>{isAuthenticated ? "" : authLinks}</div>
-      </div> */}
-      {carousel}
-    </div>
-  );
-};
-
-export default Landing;
+export default Carousel
